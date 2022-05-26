919dd7839a92698a6325a3ba515fb0192795a4f2
Çağlar <caglarjv@gmail.com>
2022-05-26 16:09:52 +0300

    Created WRONG
    forfreedoms&dreams

---
+++ kodluyoruzodev/src/kodluyoruzodev/wrong.java
@@ -0,0 +1,74 @@
+package cglrjv.freedom;
+
+import java.util.Scanner;
+
+public class wrong {
+    public static void main(String[] args) {
+
+        // I'm trying to write the code of my life. I believe. I will do.
+        // Can you understand what that means?
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println( "It may not always be written as you know it. This is bigger than dreams. Yea.. ? press anything ;)");
+        String Yea = inp.nextLine();
+
+        System.out.println(
+                    "I was born with the wrong sign\n" +
+                        "In the wrong house\n" +
+                        "With the wrong ascendancy\n" +
+                        "I took the wrong road\n" +
+                        "That led to\n" +
+                        "The wrong tendencies\n" +
+                        "I was in the wrong place\n" +
+                        "At the wrong time\n" +
+                        "For the wrong reason\n" +
+                        "And the wrong rhyme\n" +
+                        "On the wrong day\n" +
+                        "Of the wrong week\n" +
+                        "Used the wrong method\n" +
+                        "With the wrong technique\n" +
+                        "Wrong\n" +
+                        "Wrong\n" +
+                        "\n" +
+                        "There's something wrong with me chemically\n" +
+                        "Something wrong with me inherently\n" +
+                        "The wrong mix\n" +
+                        "In the wrong genes\n" +
+                        "I reached the wrong ends\n" +
+                        "By the wrong means\n" +
+                        "It was the wrong plan\n" +
+                        "In the wrong hands\n" +
+                        "The wrong theory\n" +
+                        "For the wrong man\n" +
+                        "The wrong eyes\n" +
+                        "On the wrong prize\n" +
+                        "The wrong questions\n" +
+                        "With the wrong replies\n" +
+                        "Wrong\n" +
+                        "Wrong\n" +
+                        "\n" +
+                        "I was marching to the wrong drum\n" +
+                        "With the wrong scum\n" +
+                        "Pissing out the wrong energy\n" +
+                        "Using all the wrong lines\n" +
+                        "And the wrong signs\n" +
+                        "With the wrong intensity\n" +
+                        "I was on the wrong page\n" +
+                        "Of the wrong book\n" +
+                        "With the wrong rendition\n" +
+                        "Of the wrong look\n" +
+                        "With the wrong moon\n" +
+                        "Every wrong night\n" +
+                        "With the wrong tune playing\n" +
+                        "Till it sounded right, yeah\n" +
+                        "\n" +
+                        "Wrong\n" +
+                        "Wrong\n" +
+                        "Too long\n"
+        );
+
+        System.out.println( "Respect Depech Mode");
+
+    }
+}
+
